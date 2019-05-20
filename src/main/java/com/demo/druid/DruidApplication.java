5e5f6426ecd5d4ead0fe83dcd8509735efd00eca
zhangxiaoxiang <1090510535@qq.com>
2019-05-20 20:40:59 +0800

    添加注释

--- src/main/java/com/demo/druid/DruidApplication.java
+++ src/main/java/com/demo/druid/DruidApplication.java
@@ -8,6 +8,10 @@ public class DruidApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DruidApplication.class, args);
+        System.out.println("注意你配置的端口号");
+        System.out.println("http://localhost:8080/druid/login.html");
+        System.out.println("http://localhost:8080/druid/index.html");
+
     }
 
 }
